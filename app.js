d52c7d9e328c6f3105945c995004b1be5a76b79f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-30 20:10:50 +0200

    the document object as a js representation of the web page

---
+++ app.js
@@ -0,0 +1,4 @@
+// document object the js representation of the web pages content
+// it is the entry point to the DOM and has properties and methods
+// to manipulate the DOM
+console.dir(document); // log the document object
