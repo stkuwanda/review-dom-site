8e8e15d87e00e560a88567cc3c3fac8d9eca15e9
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-30 20:38:50 +0200

    refined commentary on the DOM and the document object

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// document object the js representation of the web pages content
-// it is the entry point to the DOM and has properties and methods
-// to manipulate the DOM
+// The DOM itself is a bunch of objects that can be interacted with
+// via JS. It is a JS window into the contents of a webpage.
+
+// The document object is the entry point into the world of the DOM. It contains representations of all the content on a page, plus lots of useful methods and properties
 console.dir(document); // log the document object
