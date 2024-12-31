59e253fd90d19d81961d8828ac02040fab6a62b4
Simbarashe Kuwanda <stkuwanda@gmail.com>
2024-12-31 08:03:42 +0200

    getElementById - grab object of element by using the id attribute

--- app.js
+++ app.js
@@ -5,3 +5,7 @@
 // It contains representations of all the content on a page, plus lots 
 // of useful methods and properties
 console.dir(document); // log the document object
+
+// getElementById - to get object representation of an element
+const image = document.getElementById('banner');
+console.dir(image); // log the document object
